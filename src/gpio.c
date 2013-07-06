f296edaf733551cbb31a5dffd6d059ca7ab0f38b
Alan Barr <alanbarr2002@hotmail.com>
2013-07-06 20:03:17 +0100

    Added include files for gpio.c and i2c.c

--- src/gpio.c
+++ src/gpio.c
@@ -25,39 +25,7 @@
  *
  */
 
-/* TODO the following has grown enough it requires its own private header */
-#include "rpiGpio.h"
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <unistd.h>
-#include <errno.h>
-#include <stdio.h>
-#include <time.h>
-
-/** The size the GPIO mapping is required to be. GPPUDCLK1_OFFSET is the last
- ** register offset of interest. */
-#define GPIO_MAP_SIZE               (GPPUDCLK1_OFFSET)
-
-/** Number of GPIO pins which are available on the Raspberry Pi. */
-#define NUMBER_GPIO                 17
-
-/** Delay for changing pullup/pulldown resistors. It should be at least 150
- ** cycles which is 0.6 uS (1 / 250 MHz * 150).  (250 Mhz is the core clock)*/
-#define RESISTOR_SLEEP_US           1
-
-/** @brief GPSET_0 register */
-#define GPIO_GPSET0     *(gGpioMap + GPSET0_OFFSET / sizeof(uint32_t))
-/** @brief GPIO_GPCLR0 register */
-#define GPIO_GPCLR0     *(gGpioMap + GPCLR0_OFFSET / sizeof(uint32_t))
-/** @brief GPIO_GPLEV0 register */
-#define GPIO_GPLEV0     *(gGpioMap + GPLEV0_OFFSET / sizeof(uint32_t))
-/** @brief GPIO_GPPUD register */
-#define GPIO_GPPUD      *(gGpioMap + GPPUD_OFFSET / sizeof(uint32_t))
-/** @brief GPIO_GPPUDCLK0 register */
-#define GPIO_GPPUDCLK0  *(gGpioMap + GPPUDCLK0_OFFSET / sizeof(uint32_t))
+#include "gpio.h"
 
 /* Local / internal prototypes */
 static errStatus gpioValidatePin(int gpioNumber);
@@ -551,6 +519,5 @@ static errStatus gpioValidatePin(int gpioNumber)
 
     return rtn;
 }
-
 
 
--- src/i2c.c
+++ src/i2c.c
@@ -21,49 +21,13 @@
  *
  */
 
-/* TODO the following has grown enough it requires its own private header */
-#include "rpiGpio.h"
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <unistd.h>
-#include <errno.h>
-#include <stdio.h>
-#include <time.h>
-
-/** @brief The size the I2C mapping is required to be. */
-#define I2C_MAP_SIZE                BSC_DEL_OFFSET
-
-/** @brief Default I2C clock frequency (Hertz) */
-#define I2C_DEFAULT_FREQ_HZ         100000
-
-/** @brief nano seconds in a second */
-#define NSEC_IN_SEC                 1000000000
-
-/** @brief Clock pulses per I2C byte - 8 bits + ACK */
-#define CLOCKS_PER_BYTE             9
+#include "i2c.h"
 
 /** @brief Pointer which will be mmap'd to the I2C memory in /dev/mem */
 static volatile uint32_t * gI2cMap = NULL;
 
 /** @brief The time it takes ideally transmit 1 byte with current I2C clock */
 static int i2cByteTxTime_ns;
-
-/** @brief BSC_C register */
-#define I2C_C                       *(gI2cMap + BSC_C_OFFSET / sizeof(uint32_t))
-/** @brief BSC_DIV register */
-#define I2C_DIV                     *(gI2cMap + BSC_DIV_OFFSET / sizeof(uint32_t))
-/** @brief BSC_A register */
-#define I2C_A                       *(gI2cMap + BSC_A_OFFSET / sizeof(uint32_t))
-/** @brief BSC_DLEN register */
-#define I2C_DLEN                    *(gI2cMap + BSC_DLEN_OFFSET / sizeof(uint32_t))
-/** @brief BSC_S register */
-#define I2C_S                       *(gI2cMap + BSC_S_OFFSET / sizeof(uint32_t))
-/** @brief BSC_FIFO register */
-#define I2C_FIFO                    *(gI2cMap + BSC_FIFO_OFFSET / sizeof(uint32_t))
-
 
 /**
  * @brief       Initial setup of I2C functionality.

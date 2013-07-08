7949a5692171a389780dfd6c51925c4150ce1cc6
Alan Barr <alanbarr2002@hotmail.com>
2013-07-08 21:43:40 +0100

    Fixed issue with documentation on i2c page.
    <pre> issue with diagram.

--- search/all_73.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['scl',['SCL',['../i2c_8c.html#ab5ffc4751921608954bb7a5687566b2d',1,'i2c.c']]],
-  ['sda',['SDA',['../i2c_8c.html#a6890442e1cc24a0d61597a13576b8727',1,'i2c.c']]]
-];
